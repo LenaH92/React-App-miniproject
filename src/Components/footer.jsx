79e3ce179177ed999563342abb830b52b3013ee1
Mariia Tararaeva <mariatararaeva@icloud.com>
2024-12-10 17:02:48 +0100

    new files for nav and footer

---
+++ src/Components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ padding: '1rem', textAlign: 'center', backgroundColor: '#f1f1f1' }}>
+      <a href="https://github.com/LenaH92/React-App-miniproject.git" target="_blank" rel="noopener noreferrer">
+        GitHub Repository
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#282c34', color: 'white' }}>
+      <h1>Mini React App</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
