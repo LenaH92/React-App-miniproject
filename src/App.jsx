23f88f87f4a7879b61df61cf83aa9d5d6b36854e
Lena Cortes <lena.cortes@usal.es>
2024-12-11 12:24:20 +0100

    returned sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
 import './App.css';
 import Navbar from "./Components/navbar";
 import Sidebar from "./Components/sidebar";
 import Footer from "./Components/footer"; 
+//
+//import { useState, useEffect } from 'react'
+
+  
+
 
 function App() {
+
+  /* const [data, setData] = useState({})
+
+  useEffect(() => {
+    fetch("https://gist.github.com/ironhack-edu/8b7c90c909f70039e9f21c8ee191b0e5")
+    .then(res => res.json())
+    .then(data => setData(data))
+  }, []) */
+
 
   return (
     <div className='App'>
--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,8 +1,10 @@
 function Sidebar(){
-    <div className="sidebar">
-        <a href="https://www.ejemplo.com">Home</a>
-        <a href="https://www.ejemplo.com">About</a>
-    </div>
+    return (<div className="sidebar">
+       
+           <a href="https://www.ejemplo.com">Home</a>
+           <a href="https://www.ejemplo.com">About</a> 
+        
+    </div>)
 }
 
 
