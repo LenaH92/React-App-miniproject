d423439dabf488600b14ccdc36cb9d4778f302e1
Mariia Tararaeva <mariatararaeva@icloud.com>
2024-12-11 20:29:06 +0100

    delete button

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable react/prop-types */
-
+import { useState } from "react";
 import ListItem from "./List_Item";
 
 const List = ({array}) => {
-  return (
+    const [list, setList] = useState(array);
+    const handleDelete = (id) => {
+        const updatedList = list.filter((item) => item.id !== id); // Filter out the item with the given id
+        setList(updatedList); // Update the state
+      };
+    return (
     <div>
       <h1>List</h1>
       <ul style={{listStyleType:"none", gap:"1em", display:"flex", justifyContent:"space-evenly", flexWrap:"wrap"}}>
         {array.map((el) =>
-         <ListItem key={el.id} element={el}/>
+         <ListItem key={el.id} element={el} onDelete={handleDelete}/>
         )}
       </ul>
     </div>
--- src/Components/List_Item.jsx
+++ src/Components/List_Item.jsx
@@ -1,20 +1,21 @@
 /* eslint-disable react/prop-types */
 
-const ListItem = ({ element }) => {
+const ListItem = ({ element, onDelete }) => {
   return (
     <>
       {element.price < 200 ? (
         <li style={{border: "1px solid black", height:"200px"}} >
           <p>{element.title}</p>
           <p>${element.price}</p>  ✅
+          <button onClick={() => onDelete(element.id)}>Delete</button> 
         </li>
       ) : (
         <li >
           {element.title} ${element.price} ❌
+          <button onClick={() => onDelete(element.id)}>Delete</button>
         </li>
-
       )}
     </>
   );
-};
+}; 
 export default ListItem;
